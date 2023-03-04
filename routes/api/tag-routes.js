d41c09898432cbe7c90e35a38dffb16ba7142266
Sidney Basa <basa206@hotmail.com>
2023-03-03 23:08:31 -0800

    added tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,102 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', (request, response) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include:[Product]
+  }).then(data=>{
+    response.json(data)
+}).catch(error=>{
+    console.log(error);
+    response.status(500).json({
+        msg:"an error occurred",
+        error:error
+    })
+})
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (request, response) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(request.params.id, {
+    include:[{
+      model:Product,
+      include:[Tag]
+  }]
+  }).then(data=>{
+    if(data){
+       return  response.json(data);
+    } else {
+      response.status(404).json({
+            msg:"no such record"
+        })
+    }
+}).catch(err=>{
+    console.log(err);
+    response.status(500).json({
+        msg:"an error occurred",
+        error:error
+    })
+})
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (request, response) => {
   // create a new tag
+  Tag.create(request.body).then(data=>{
+    response.status(201).json(data)
+}).catch(err=>{
+    console.log(error);
+    response.status(500).json({
+        msg:"an error occurred",
+        error:error
+    })
+})
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', (request, response) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tag_name:request.body.tag_name
+},{
+    where:{
+        id:request.params.id
+    }
+}).then(data=>{
+    if(data[0]){
+        return response.json(data)
+    } else {
+        return response.status(404).json({msg:"no such record"})
+    }
+}).catch(error=>{
+    console.log(error);
+    response.status(500).json({
+        msg:"an error occurred",
+        error:error
+    })
+})
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (request, response) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+        id:request.params.id
+    }
+}).then(data=>{
+    if(data){
+        return response.json(data)
+    } else {
+        return response.status(404).json({msg:"no such record"})
+    }
+}).catch(err=>{
+    console.log(error);
+    response.status(500).json({
+        msg:"an error occurred",
+        error:error
+    })
+})
 });
 
 module.exports = router;
